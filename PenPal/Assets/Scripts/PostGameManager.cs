632eb27d81cd2309b703f0c138a2f80a3f555529
Jake <jesmith24@comcast.net>
2018-05-20 14:47:13 -0700

    post game complete, links to credits

--- PenPal/Assets/Scripts/PostGameManager.cs
+++ PenPal/Assets/Scripts/PostGameManager.cs
@@ -11,39 +11,26 @@ public class PostGameManager : MonoBehaviour {
     //public Text cutSceneTitle;
     public float timer;
     public float duration;
-    public int switchPrompt;      
-    
-    private GraphicColorLerp flavorColorLerp;
-    private GraphicColorLerp flavorColorLerp2;
-    List<string> cutSceneQueue;
+	List<string> cutSceneQueue;
 
     public Text prompt1;
     public Text prompt2;
     private GraphicColorLerp colorLerp1;
     private GraphicColorLerp colorLerp2;
-    public float holdDuration;
-    public float gapBetweenSentences;
-    private float[] timers;
 	void Start () {
         cutSceneQueue = new List<string>(){"I got a letter in the mail today.", "I recognized the handwriting."};
         colorLerp1 = prompt1.gameObject.GetComponent<GraphicColorLerp>();
 		colorLerp2 = prompt2.gameObject.GetComponent<GraphicColorLerp>();
 		prompt1.text = cutSceneQueue[0];
 		prompt2.text = cutSceneQueue[1];
-		colorLerp1.duration = 5f;
-		colorLerp2.duration = 5f;
-		StartCoroutine(textLoop(colorLerp1, colorLerp2, 10f));
+		colorLerp1.duration = 2f;
+		colorLerp2.duration = 2f;
+		colorLerp2.initialDelay = 4f;
+		StartCoroutine(textLoop(colorLerp1));
+		StartCoroutine(textLoop(colorLerp2));
+		StartCoroutine(endGame(10f));
 	}
 
-    //comment this coroutine to get second text fade in
-    IEnumerator Example()
-    {
-        flavorColorLerp.startColorChange();
-        yield return new WaitForSeconds(3);
-        flavorColorLerp2.startColorChange();
-        yield return new WaitForSeconds(3);
-    }
-	
 	// Update is called once per frame
 	void Update () {
         /*
@@ -90,11 +77,13 @@ public class PostGameManager : MonoBehaviour {
         }       
         */
 	}
-    private IEnumerator textLoop(GraphicColorLerp colorLerp1, GraphicColorLerp colorLerp2, float delay)
+    private IEnumerator textLoop(GraphicColorLerp colorLerp)
     {
-        colorLerp1.startColorChange();
-		yield return new WaitForSeconds(delay);
-		//colorLerp2.startColorChange();
-		//yield return new WaitForSeconds(delay);
+        colorLerp.startColorChange();
+		yield return null;
     }
+	private IEnumerator endGame(float delay) {
+		yield return new WaitForSeconds(delay);
+		SceneManager.LoadScene("CreditsScene");
+	}
 }
