49bbf1f04933eed937b1ebcd2344c0217aae47bb
Kyle Wong <kylewonga@gmail.com>
2018-05-19 17:12:31 -0700

    Updated letter writing scene

--- PenPal/Assets/Scripts/LetterWritingController.cs
+++ PenPal/Assets/Scripts/LetterWritingController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LetterWritingController : MonoBehaviour {
 
@@ -28,7 +29,7 @@ public class LetterWritingController : MonoBehaviour {
     LetterEvent chosenLE;
     private int lineCount;
     private bool waitingForInput;
-
+    public string cutScene;
     private void Awake()
     {
         headerRevealer.charDelay = charDelay;
@@ -179,5 +180,9 @@ public class LetterWritingController : MonoBehaviour {
     public State getState()
     {
         return state;
+    }
+    public void goToCutscene()
+    {
+        SceneManager.LoadScene(cutScene);
     }
 }
