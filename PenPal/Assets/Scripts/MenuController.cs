64dd86bafcdf017ae8c67f85e570bc63d766d8c1
Kyle Wong <kylewonga@gmail.com>
2018-05-20 12:06:41 -0700

    Added name input field to main menu

--- PenPal/Assets/Scripts/MenuController.cs
+++ PenPal/Assets/Scripts/MenuController.cs
@@ -13,12 +13,42 @@ public class MenuController : MonoBehaviour {
     public EventSystem eventSystem;
     public GraphicColorLerp fadeToBlack;
     public bool allowInput = true;
+    public GameObject mainMenuButtons;
+    public Canvas inputCanvas;
+    public InputField nameBox;
+    public GameObject playButton;
+    public enum State
+    {
+        MainMenu,
+        AtInputField,
+    }
+    private State state = State.MainMenu;
 	void Start () {
+        inputCanvas.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        switch (state)
+        {
+            case State.MainMenu:
+                break;
+            case State.AtInputField:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    SceneManager.LoadScene("MainMenu");
+                    //lazy way to go back
+                }
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    startGame();
+                }
+                if (Input.GetKeyDown(KeyCode.Tab))
+                {
+                    eventSystem.SetSelectedGameObject(playButton);
+                }
+                break;
+        }
 	}
 
 
@@ -28,9 +58,17 @@ public class MenuController : MonoBehaviour {
     {
         if (!allowInput)
             return;
+        GameManager.playerName = nameBox.text;
         allowInput = false;
         eventSystem.SetSelectedGameObject(null);
         StartCoroutine(loadAfterDelay(startGameScene, fadeToBlack.duration));
+    }
+    public void goToInputField()
+    {
+        mainMenuButtons.SetActive(false);
+        inputCanvas.enabled = true;
+        eventSystem.SetSelectedGameObject(nameBox.gameObject);
+       
     }
     public void goToCredits()
     {
