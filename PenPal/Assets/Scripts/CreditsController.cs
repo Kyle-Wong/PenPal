2e3a04a38e1bd84455a7e1c6ef2434207829d384
Kyle Wong <kylewonga@gmail.com>
2018-05-19 23:09:29 -0700

    Main Menu and Credits implemented

---
+++ PenPal/Assets/Scripts/CreditsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsController : MonoBehaviour {
+
+	// Use this for initialization
+    public UIRevealer[] nameRevealer;
+    public UIRevealer[] roleRevealer;
+    public float initialDelay;
+    public float repeatDelay;
+	void Start () {
+        StartCoroutine(revealCredits());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    private IEnumerator revealCredits()
+    {
+        yield return new WaitForSeconds(initialDelay);
+        for(int i = 0; i < nameRevealer.Length; i++)
+        {
+            nameRevealer[i].revealUI();
+            roleRevealer[i].revealUI();
+            yield return new WaitForSeconds(repeatDelay);
+        }
+    }
+    public void goToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ PenPal/Assets/Scripts/MenuController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    // Use this for initialization
+    public string startGameScene;
+    public string creditsScene;
+    public EventSystem eventSystem;
+    public GraphicColorLerp fadeToBlack;
+    public bool allowInput = true;
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+    public void startGame()
+    {
+        if (!allowInput)
+            return;
+        allowInput = false;
+        eventSystem.SetSelectedGameObject(null);
+        StartCoroutine(loadAfterDelay(startGameScene, fadeToBlack.duration));
+    }
+    public void goToCredits()
+    {
+        SceneManager.LoadScene(creditsScene);
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+    public IEnumerator loadAfterDelay(string sceneName, float delay)
+    {
+        fadeToBlack.startColorChange();
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
+}
