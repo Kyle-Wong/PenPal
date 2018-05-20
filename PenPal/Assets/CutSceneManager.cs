4afae4d0d2d0229232b2d13b4ada6b83831e44d2
Steven Kha <stevenvkha@gmail.com>
2018-05-19 17:09:01 -0700

    fixed variables and names

--- PenPal/Assets/CutSceneManager.cs
+++ PenPal/Assets/CutSceneManager.cs
@@ -8,7 +8,7 @@ public class CutSceneManager : MonoBehaviour {
 	// Use this for initialization
     public Image background;
     public Text cutSceneTitle;
-    public Text flavorText;
+    public Text narrativePrompt;
     public float timer;
     public float duration;
     private GraphicColorLerp titleColorLerp;
@@ -20,29 +20,39 @@ public class CutSceneManager : MonoBehaviour {
         cutSceneQueue = GameManager.narrativeQueue;
         curIndex = cutSceneQueue.Dequeue();
         cutSceneTitle.text = "Chapter 1";
-        flavorText.text = " ";
+        narrativePrompt.text = " ";
         //timer = 5;
         titleColorLerp = cutSceneTitle.GetComponent<GraphicColorLerp>();
         titleColorLerp.duration = 5;
         titleColorLerp.startColorChange();
-        flavorColorLerp = flavorText.GetComponent<GraphicColorLerp>();
-        flavorColorLerp.initialDelay = 5;
-        flavorColorLerp.duration = 5;
-        flavorColorLerp.startColorChange();
+        flavorColorLerp = narrativePrompt.GetComponent<GraphicColorLerp>();
+        //flavorColorLerp.initialDelay = 5;
+        //flavorColorLerp.duration = 5;
+        //flavorColorLerp.startColorChange();
         duration = 5; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-       
-       
-        timer += Time.deltaTime;
-        while (curIndex.type.text != "EOL")
-            if (timer >= duration)
-            {
-                //cutSceneTitle.text = " ";                  
-                flavorText.text = curIndex.text;
-            }
 
+
+        timer += Time.deltaTime;
+        if (timer >= duration)
+        {
+            //cutSceneTitle.text = " ";                  
+            narrativePrompt.text = curIndex.text;
+        }
+
+
+        //while (curIndex.text != " ")
+        //{
+        //    if (timer >= duration)
+        //    {
+        //        //cutSceneTitle.text = " ";                  
+        //        narrativePrompt.text = curIndex.text;
+        //        curIndex = cutSceneQueue.Dequeue();
+        //    }
+
+        //}
 	}
 }
