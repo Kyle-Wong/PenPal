0b42654a09c1a5643d85a87dd2cabc588f6192e5
Steven Kha <stevenvkha@gmail.com>
2018-05-20 15:46:32 -0700

    meta files conflict

---
+++ PenPal/Assets/Prefabs/GameMusicScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusicScript : MonoBehaviour {
+
+    public AudioClip[] sounds;
+    public AudioSource music;
+    Queue<LetterEvent> letterQueue;
+    LetterEvent curIndex;
+    int currentGroup = 1;
+	// Use this for initialization
+	void Start () {
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int groupIndex = GameManager.playerQueue.Peek().group;
+		if(currentGroup != groupIndex){
+            music.Stop();
+            music.clip = sounds[groupIndex-1];
+            music.Play();
+        }
+        currentGroup = groupIndex;
+	}
+
+    private void playsound(int index)
+    {
+        music.clip = sounds[index];
+        music.Play();
+    }
+}
