24a5240e401305a6a2624abb4bfd454e45c4b68b
Kyle Wong <kylewonga@gmail.com>
2018-05-20 12:49:24 -0700

    Added autoscrolling

--- PenPal/Assets/Scripts/PaperScroller.cs
+++ PenPal/Assets/Scripts/PaperScroller.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PaperScroller : MonoBehaviour {
 
@@ -11,13 +12,34 @@ public class PaperScroller : MonoBehaviour {
     public float maxY;
     private float mouseDiff;
     private Camera mainCam;
-	void Start () {
+    public TextMesh textMesh;
+    private float oldHeight;
+    private float newHeight;
+    public bool autoScroll = false;
+    public float autoScrollingStartY;
+    private bool firstScroll = true;
+    private float scrollHeight = 0;
+    private void Awake()
+    {
+        LetterEvent LE = new LetterEvent();
+        LE.type = LetterEvent.Type.SENTENCE;
+        LE.text = " a asd fsd asdf dfsa das fsdadfas fad afsd f ad fad asfd fasd asfd fasd asfdf sd" +
+            "\n fasd sdfa asdf fsd fsda asdf fsda fd sdf fsd fsd fa adsf asdfafsd ";
+        GameManager.playerQueue.Enqueue(LE);
+    }
+    void Start () {
         gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<LetterWritingController>();
         if (gameController == null) //i assume if this fails, i need to find ResponseWritingController
                 gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<ResponseWritingController>();
         if (gameController == null)
                 gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<MadlibsController>();
         mainCam = Camera.main;
+        if (autoScroll)
+        {
+            newHeight = textMesh.GetComponent<Renderer>().bounds.extents.y;
+            oldHeight = newHeight;
+        }
+
 	}
 	
 	// Update is called once per frame
@@ -49,6 +71,22 @@ public class PaperScroller : MonoBehaviour {
         if (transform.position.y < minY)
         {
             transform.position = new Vector3(transform.position.x, minY, transform.position.z);
+        }
+      
+        if (autoScroll)
+        {
+            newHeight = textMesh.GetComponent<Renderer>().bounds.extents.y;
+            
+            if (newHeight > autoScrollingStartY && oldHeight < newHeight)
+            {
+                if (firstScroll)
+                {
+                    scrollHeight = (newHeight - oldHeight);
+                    firstScroll = false;
+                }
+                transform.position += Vector3.up * scrollHeight;
+            }
+            oldHeight = newHeight;
         }
     }
     
